116ca295db3c11041f3c14cef3981553551c6602
FabioMG1A <34440826+FabioMG1A@users.noreply.github.com>
2018-12-07 11:18:17 +0100

    Add files via upload

---
+++ Eindproject/test/test3.java
@@ -0,0 +1,23 @@
+function resetMoves(){
+  // reset moves
+    moves = 0;
+    counter.innerHTML = moves;
+    }
+
+
+function moveCounter(){
+if (moves > 8 && moves < 12){
+        for( i= 0; i < 3; i++){
+            if(i > 1){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+    else if (moves > 13){
+        for( i= 0; i < 3; i++){
+            if(i > 0){
+                stars[i].style.visibility = "collapse";
+            }
+        }
+    }
+}
